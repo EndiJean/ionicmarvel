5e24865c624a6115a555324268efe71333b74c6d
Endi Jean <endijean91@gmail.com>
2022-05-02 22:14:44 -0300

    implementados metodos do provider character.service

--- src/app/provider/character.service.ts
+++ src/app/provider/character.service.ts
@@ -9,6 +9,57 @@ export class CharacterService {
 
   constructor(private service: ServiceService) { }
 
+  public getCharecterById(id: number){
+    return new Promise((ret) => {
+      this.service.getDados('/v1/public/characters/' + id, '')
+      .then((data: any) => {
+
+          if(data && data.data && data.data.results){
+            ret(data.data.results);
+
+          } else {
+
+            ret([]);
+
+          }
+      })
+    })
+  }
+
+  public getAllCharecters(
+    pagination: PaginationComponent,
+    filter: string){
+      let strFilter = '';
+
+      let param = '&limit=' + pagination.getLimit() + '&offset' + pagination.getOffset() + strFilter;
+
+      return new Promise((ret) => {
+        this.service.getDados('/v1/public/characters',  param).then((data:any) => {
+          if(data && data.data && data.data.results){
+            this.updatePagination(pagination, data.data);
+            ret(data.data.results);
+
+          } else {
+            ret([]);
+               
+          }
+        })
+      })
+    }
+
+    public getComicsByCharacter(id: number){
+      return new Promise((ret) => {
+        this.service.getDados('/v1/public/characters/' + id + '/comics', '').then((data:any) => {
+          if(data && data.data && data.data.results){
+            ret(data.data.results);
+          } else {
+            ret([]);
+          }
+        })
+
+      })
+    }
+
   /* atualiza as informações de paginação */
   private updatePagination(pagination: PaginationComponent, data: any){
     pagination.setTotal(data.total);
